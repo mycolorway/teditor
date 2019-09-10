64873f31fc5e7960ab19471f4d0ed78743672cc1
farthinker <farthinker@gmail.com>
2019-09-10 10:45:02 +0800

    CHG: remove widget requires

--- src/code-block/codeblock.js
+++ src/code-block/codeblock.js
@@ -1,5 +1,4 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-import Widget from '@ckeditor/ckeditor5-widget/src/widget';
 
 import CodeBlockEditing from './codeblockediting';
 import CodeBlockUI from './codeblockui';
@@ -7,6 +6,6 @@ import './codeblocklocale';
 
 export default class Emoji extends Plugin {
   static get requires() {
-    return [Widget, CodeBlockEditing, CodeBlockUI];
+    return [CodeBlockEditing, CodeBlockUI];
   }
 }
