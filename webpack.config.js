b8c193e1b3360f627daa2bf99f7ed5fb355dcf9a
farthinker <farthinker@gmail.com>
2019-09-19 18:03:14 +0800

    add: custom styles and icons

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 /* eslint-env node */
 
 const path = require('path');
+const webpack = require('webpack');
 const { styles } = require('@ckeditor/ckeditor5-dev-utils');
 const TerserPlugin = require('terser-webpack-plugin');
 const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin');
+
+const customIcons = [
+  'underline', 'align-center', 'align-left', 'align-right', 'align-justify', 'attachment', 'bold',
+  'bulletedlist', 'font-color', 'font-size', 'image', 'indent', 'italic', 'link-resource',
+  'link', 'unlink', 'numberedlist', 'outdent', 'quote', 'strikethrough', 'table', 'dropdown-arrow',
+  'pencil', 'remove-format',
+];
 
 module.exports = {
   mode: 'development',
@@ -34,6 +42,15 @@ module.exports = {
     new CKEditorWebpackPlugin({
       language: 'zh-CN',
     }),
+    new webpack.NormalModuleReplacementPlugin(
+      /ckeditor5-[^/]+\/theme\/icons\/[^/]+\.svg$/,
+      (result) => {
+        const match = result.request.match(/ckeditor5-[^/]+\/theme\/icons\/([^/]+)\.svg$/);
+        if (match && customIcons.includes(match[1])) {
+          result.resource = path.resolve(__dirname, `src/theme/icons/${match[1]}.svg`); // eslint-disable-line
+        }
+      },
+    ),
   ],
   module: {
     rules: [{
