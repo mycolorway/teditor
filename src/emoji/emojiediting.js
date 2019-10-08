8d86db3f16699cb11c88e1d3be84de4280471f1c
farthinker <farthinker@gmail.com>
2019-10-08 22:31:08 +0800

    change: render emoji as widget

--- src/emoji/emojiediting.js
+++ src/emoji/emojiediting.js
@@ -1,12 +1,16 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import { toWidget, viewToModelPositionOutsideModelElement } from '@ckeditor/ckeditor5-widget/src/utils';
 import InsertEmojiCommand from './commands/insertemojicommand';
 
 import './theme/emoji.css';
 
+const EMOJI_IMAGE_CLASS = 'teditor-emoji';
+const EMOJI_WRAPPER_CLASS = 'teditor-emoji-wrapper';
+
 function convertModelElement(modelElement, viewWriter) {
   return viewWriter.createEmptyElement('img', {
     src: modelElement.getAttribute('src'),
-    class: 'teditor-emoji',
+    class: EMOJI_IMAGE_CLASS,
   });
 }
 
@@ -20,6 +24,14 @@ export default class EmojiEditing extends Plugin {
   init() {
     this.defineSchema();
     this.defineConverters();
+
+    this.editor.editing.mapper.on(
+      'viewToModelPosition',
+      viewToModelPositionOutsideModelElement(
+        this.editor.model,
+        (viewElement) => viewElement.hasClass(EMOJI_WRAPPER_CLASS),
+      ),
+    );
 
     this.editor.commands.add('insertEmoji', new InsertEmojiCommand(this.editor));
   }
@@ -41,11 +53,11 @@ export default class EmojiEditing extends Plugin {
     conversion.for('upcast').elementToElement({
       view: {
         name: 'img',
-        classes: 'teditor-emoji',
+        classes: EMOJI_IMAGE_CLASS,
       },
       model: convertViewElement,
       converterPriority: 'high',
-    }).elementToElement({
+    }).elementToElement({ // for backward compatibility
       view: {
         name: 'img',
         attributes: {
@@ -54,11 +66,46 @@ export default class EmojiEditing extends Plugin {
       },
       model: convertViewElement,
       converterPriority: 'high',
+    }).add((dispatcher) => {
+      dispatcher.on('element:span', (event, data, conversionApi) => {
+        if (!conversionApi.consumable.consume(data.viewItem, {
+          name: true, classes: EMOJI_WRAPPER_CLASS,
+        })) {
+          return;
+        }
+
+        const viewImage = Array.from(data.viewItem.getChildren()).find((viewChild) => viewChild.is('img'));
+        if (!viewImage || !viewImage.hasAttribute('src') || !conversionApi.consumable.test(viewImage, { name: true })) {
+          return;
+        }
+
+        const conversionResult = conversionApi.convertItem(viewImage, data.modelCursor);
+
+        // eslint-disable-next-line no-param-reassign
+        data.modelRange = conversionResult.modelRange;
+        // eslint-disable-next-line no-param-reassign
+        data.modelCursor = conversionResult.modelCursor;
+      });
     });
 
-    conversion.for('downcast').elementToElement({
+    conversion.for('dataDowncast').elementToElement({
       model: 'emoji-image',
       view: convertModelElement,
+    });
+
+    conversion.for('editingDowncast').elementToElement({
+      model: 'emoji-image',
+      view(modelElement, viewWriter) {
+        const imageElement = convertModelElement(modelElement, viewWriter);
+        const wrapperElement = viewWriter.createContainerElement('span', {
+          class: EMOJI_WRAPPER_CLASS,
+        });
+
+        viewWriter.insert(viewWriter.createPositionAt(wrapperElement, 0), imageElement);
+        return toWidget(wrapperElement, viewWriter, {
+          label: 'emoji widget',
+        });
+      },
     });
   }
 }
--- src/horizontal-rule/horizontalrulelocale.js
+++
@@ -1,5 +0,0 @@
-import { add } from '@ckeditor/ckeditor5-utils/src/translation-service';
-
-add('zh-CN', {
-  'Horizontal rule': '插入分隔线',
-});
